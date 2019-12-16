8692d71b7b62c7ba62996e464331f06240e25653
Student <example@example.com>
2019-12-16 13:28:08 -0800

    add content

--- lib/project.rb
+++ lib/project.rb
@@ -1,1 +1,40 @@
+class Characters
+  attr_reader :id, :name
+  attr_accessor :name
+  @@characters = {}
+  @@total_rows = 0
 
+  def initialize(name, id)
+    @name = name
+    @id = id || @@total_rows += 1
+  end
+
+  def save()
+    @@characters[self.id] = Characters.new(self.name, self.id)
+  end
+
+  def self.find(id)
+    @@characters[id]
+  end
+
+  def ==(character_to_compare)
+    self.name() == character_to_compare.name()
+  end
+
+  def update(name)
+    self.name = name
+    @@characters[self.id] = Characters.new(self.name,self.id)
+  end
+
+  def self.all
+    @@characters.values()
+  end
+
+  def delete()
+    @@characters.delete(self.id)
+  end
+  def self.clear
+    @@characters = {}
+    @@total_rows = 0
+  end
+end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,1 +1,73 @@
+require('rspec')
+require('project')
 
+describe '#Characters' do
+  before(:each) do
+    Characters.clear()
+  end
+
+  describe('#save') do
+    it('saves Star Wars characters') do
+      character = Characters.new('Han Solo', nil)
+      character.save()
+      character2 = Characters.new('Luke Skywalker', nil)
+      character2.save()
+      expect(Characters.all).to(eq([character, character2]))
+    end
+  end
+
+  describe('.all') do
+    it("returns an empty array when there are no characters") do
+      expect(Characters.all).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all albums") do
+      character = Characters.new("Princess Leia", nil)
+      character.save()
+      character2 = Characters.new("Darth Vader", nil)
+      character2.save()
+      Characters.clear()
+      expect(Characters.all).to(eq([]))
+    end
+  end
+
+  describe('#==') do
+    it("is the same album if it has the same attributes as another album") do
+      character = Characters.new("Grand Moff Tarkin", nil)
+      character2 = Characters.new("Grand Moff Tarkin", nil)
+      expect(character).to(eq(character2))
+    end
+  end
+
+  describe('.find') do
+    it("finds an album by id") do
+      character = Characters.new("Darth Vader", nil)
+      character.save()
+      character2 = Characters.new("Grand Moff Tarkin", nil)
+      character2.save()
+      expect(Characters.find(character.id)).to(eq(character))
+    end
+  end
+
+  describe('#update') do
+    it("updates an album by id") do
+      character = Characters.new("Darth Vader", nil)
+      character.save()
+      character.update("Chewbacca")
+      expect(character.name).to(eq("Chewbacca"))
+    end
+  end
+
+  describe('#delete') do
+    it("deletes an album by id") do
+      character = Characters.new("Darth Vader", nil)
+      character.save()
+      character2 = Characters.new("Princess Leia", nil)
+      character2.save()
+      character.delete()
+      expect(Characters.all).to(eq([character2]))
+    end
+  end
+end
